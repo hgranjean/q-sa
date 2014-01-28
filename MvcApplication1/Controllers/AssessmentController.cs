cd28972b925b4ee182652800a7dc4665765de0a3
schmidtaa <me@alexschmidt.net>
2014-01-27 21:56:33 -0600

    Added assessment

---
+++ MvcApplication1/Controllers/AssessmentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using MvcApplication1.Filters;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    [Authorize]
+    [InitializeSimpleMembership]
+    public class AssessmentController : Controller
+    {
+        public ActionResult Index()
+        {   
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(AssessmentViewModel viewModel)
+        {
+            return View("Details");
+        }
+
+        public ActionResult Details(int assessmentId)
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcApplication1/Models/AssessmentModels.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using Repository;
+using Runtime;
+using Entity = Runtime.Entity;
+
+namespace MvcApplication1.Models
+{
+    public class AssessmentViewModel : ViewModelBase
+    {
+        //Entity member variable should *not* be static - its lifetime is for the request only
+        public Entity Assessment { get; private set; }
+        // public RuleSessionState SessionState { get; private set; }
+
+        static AssessmentViewModel()
+        {
+            LoadRules();
+        }
+
+        public AssessmentViewModel()
+        {
+            Assessment = LoadAssessment();
+        }
+
+        private Entity LoadAssessment()
+        {
+            return null;
+        }
+
+        
+    }
+
+    public abstract class ViewModelBase
+    {
+        private static RuleApplicationDef rulesApp;
+        public RuleSession Session { get; private set; }
+
+        protected static void LoadRules()
+        {
+            // rulesApp = RuleApplicationDef.Load(GetRuleAppFileName());
+        }
+
+        protected string GetRuleAppFileName()
+        {
+            string appPath = HttpContext.Current.Server.MapPath("~/bin");
+            int binPos = appPath.LastIndexOf(@"\bin");
+
+            appPath = appPath.Substring(0, binPos) + @"\RuleApp\";
+            
+            return appPath + ConfigurationSettings.AppSettings.Get("RuleApp");
+        }
+
+        private string GetRuleAppStateFileName()
+        {
+            string appPath = HttpContext.Current.Server.MapPath("~/bin");
+            int binPos = appPath.LastIndexOf(@"\bin");
+
+            appPath = appPath.Substring(0, binPos) + @"\RuleApp\";
+
+            return appPath + ConfigurationSettings.AppSettings.Get("RuleAppState");
+        }
+
+        /*
+        /// <summary>
+        /// Create <see cref="RuleSession"/> for use during this page request only, 
+        ///	using persistent <see cref="RuleSessionState"/> from ASP.NET session state if any.
+        /// If no ASP.NET session state yet stored, a new one is created here.
+        /// </summary>
+        /// <returns></returns>
+        protected RuleSession CreateRuleSession()
+        {
+            //Get from ASP.NET Session State
+            var state = this.SessionState;
+            if (state == null)
+            {
+                state = new RuleSessionState();
+            }
+
+            var session = this.Session;
+            if (session == null)
+            {
+                var ruleAppSource = RuleApplicationDef.Load(GetRuleAppFileName());
+
+                //Set up appropriate connection based on config setting
+                session = new RuleSession(ruleAppSource, CacheRetention.AlwaysRetain);
+                session.SaveState();
+                // Preserve the working memory between subsequent requests to engine service
+                // session.RuleExecutionSettings.WorkingMemoryBehavior = WorkingMemoryBehavior.Default;
+            }
+
+            //Store in ASP.NET Session
+            this.Session = session;
+
+            return session;
+        }*/
+
+    }
+}
