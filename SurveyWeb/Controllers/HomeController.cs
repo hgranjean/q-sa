8d966a6038e2678979f6200e5d2473ef939e7c88
schmidtaa <me@alexschmidt.net>
2014-02-25 00:08:28 -0600

    Added left menu

--- SurveyWeb/Controllers/HomeController.cs
+++ SurveyWeb/Controllers/HomeController.cs
@@ -15,14 +15,21 @@ namespace SurveyWeb.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "The application description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "The contact page.";
+
+            return View();
+        }
+
+        public ActionResult Settings()
+        {
+            ViewBag.Message = "The settings page.";
 
             return View();
         }
