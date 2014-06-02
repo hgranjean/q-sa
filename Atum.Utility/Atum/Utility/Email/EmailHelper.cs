fd0d20252cb17ede7ad806642bc9790a95e441a6
schmidtaa <me@alexschmidt.net>
2014-06-02 18:00:09 -0500

    Added Email helper

---
+++ Atum.Utility/Atum/Utility/Email/EmailHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atum.Utility
+{
+    public class EmailHelper
+    {
+        public static string GetDomainName(string value)
+        {
+            var mailAddress = new MailAddress(value);
+
+            return mailAddress.Host;
+        }
+    }
+}
