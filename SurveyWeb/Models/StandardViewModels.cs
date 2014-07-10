6917864178c894afbb8a28ddacf9f5a4d8b5a53a
hgranjean <hgranjean@yahoo.com>
2014-07-09 20:26:22 -0500

    Standard Administration

---
+++ SurveyWeb/Models/StandardViewModels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurveyWeb.Models
+{
+    public class StandardViewModels : ViewModelBase
+    {
+
+    }
+
+
+    public class StandardDocumentViewModel
+    {
+        public string Title { get; set; }
+        public List<TOCElementViewModel> TableOfContents { get; set; }
+    
+    }
+    public class TOCElementViewModel
+    {
+        public string Title { get; set; }
+        public int Level { get; set; }
+        public string Content { get; set; }
+        public string ShortContent { get; set; }
+
+
+        public string Key { get; set; }
+    }
+}
---
+++ SurveyWeb/Services/StandardsManagementServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurveyWeb.Services
+{
+    internal class StandardsManagementServices
+    {
+        internal static Models.StandardDocumentViewModel GetChapter(string chapterId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
