9e8faa2b94ab43e66c5677a813483ba1c4b2df70
schmidtaa <me@alexschmidt.net>
2014-07-14 23:30:30 -0500

    Added blank classifier test

---
+++ Atum.Domain.Test/NLP/ClassifierTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atum.Domain.Test.NLP
+{
+    [TestClass]
+    public class ClassifierTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
