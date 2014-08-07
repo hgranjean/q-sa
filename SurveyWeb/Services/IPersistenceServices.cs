8b81afd43b06a7859acbc7023d089c35e2c218d2
schmidtaa <me@alexschmidt.net>
2014-08-07 12:42:21 -0500

    Added missing interface file

---
+++ SurveyWeb/Services/IPersistenceServices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Atum.Domain.SurveyManagement;
+using SurveyWeb.Models;
+
+namespace SurveyWeb.Services
+{
+    public interface IPersistenceServices
+    {
+        IEnumerable<string> GetResponses();
+        Survey GetSurvey(int id);
+        SurveyManager GetSurveyManager(Survey survey);
+        IEnumerable<Survey> GetSurveys();
+        Surveys GetSurveys(string criteria);
+        Survey LoadSurvey(string name);
+        TracerViewModel LoadTracer(string responseId);
+        void SaveSurvey(Survey survey);
+        void SaveTracer(TracerViewModel survey);
+        void DeleteSurvey(string surveyId);
+    }
+}
