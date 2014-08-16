4c752e8340d49a7e7726e73bc31414ce3274cf30
schmidtaa <me@alexschmidt.net>
2014-08-15 22:09:42 -0500

    Adding DebugUtil.cs

---
+++ Atum.Utility/Atum/Utility/Diagnostics/DebugUtil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atum.Utility.Diagnostics
+{
+    /// <summary>
+    /// Conditional wrapper around Debug class. Only applies to Debug builds
+    /// </summary>
+    public class DebugUtil
+    {
+        [Conditional("DEBUG")]
+        public static void WriteLine(string message)
+        {
+            Debug.WriteLine(message);
+        }
+        [Conditional("DEBUG")]
+        public static void WriteLine(string message, params object[] args)
+        {
+            Debug.WriteLine(message, args);
+        }
+
+        [Conditional("DEBUG")]
+        public static void Assert(bool condition, string message)
+        {
+            Debug.Assert(condition, message);
+        }
+    }
+}
