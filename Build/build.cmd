bd6ec6e2c51e70a90001d55312f5bd5cdce41508
schmidtaa <me@alexschmidt.net>
2014-08-21 17:35:50 -0500

    Updated build scripts

--- Build/build.cmd
+++ Build/build.cmd
@@ -1,2 +1,2 @@
 SET PATH=%PATH%;C:\Windows\Microsoft.NET\Framework64\v4.0.30319
-MSBuild.exe D:\WORK\ATUM\Code\SurveySolution\Build\build.proj /t:FullBuild10 /p:Configuration=Release
+MSBuild.exe Build\build.proj /p:Configuration=Release
