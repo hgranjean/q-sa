9acbcfef39f46cffa75995a46270120dc20dff20
schmidtaa <me@alexschmidt.net>
2014-09-05 18:16:42 -0500

    Updated core components to support arjuna theme

--- SurveyWeb/App_Start/BundleConfig.cs
+++ SurveyWeb/App_Start/BundleConfig.cs
@@ -8,8 +8,10 @@ namespace SurveyWeb
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            // Fix for bundles not rendered correctly in the release version
+            // Fix for bundles not rendered correctly in the release version (V= bug)
             bundles.IgnoreList.Clear();
+
+            // AQS scripts and styles
 
             bundles.Add(new ScriptBundle("~/bundles/aqs").Include(
                         "~/Scripts/AQS/aqs.js",
@@ -17,8 +19,19 @@ namespace SurveyWeb
                         "~/Scripts/jquery-ui-1.11.0.js" 
                         ));
 
+            // AQS theme enhancements
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(                      
+                      "~/Content/site.css",
+                      "~/Content/component.css",
+                      "~/Content/Demo.css",
+                      "~/Content/Normalize.css",
+                      "~/Content/jquery-ui.css"));            
+
             //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
             //            "~/Scripts/jquery-{version}.js"));
+
+            // JQuery support for validation and unobtrusive ajax calls
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -31,18 +44,37 @@ namespace SurveyWeb
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            // Bootstrap
+
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",                      
-                      "~/Content/site.css",
-                      "~/Content/component.css",
-                      "~/Content/Demo.css",
-                      "~/Content/Normalize.css",
-                      "~/Content/jquery-ui.css"));            
-                      
+            // Base theme
+
+            bundles.Add(new StyleBundle("~/Content/basetheme").Include(
+                      "~/Content/bootstrap.css"));            
+
+            // Arjuna theme
+
+            bundles.Add(new StyleBundle("~/Content/arjunatheme").Include(
+                      "~/Content/themes/arjuna/css/bootstrap.css", // bootstrap v3.2.0
+                      "~/Content/themes/arjuna/css/font-awesome.min.css", // font awesome v4.1.0
+                      "~/Content/themes/arjuna/css/bootstrap-reset.css", // bootstrap reset
+                      "~/Content/themes/arjuna/css/arjuna.css", // arjuna
+                      "~/Content/themes/arjuna/plugins/owl.carousel/owl-carousel/owl.carousel.css", // plugins for carousel, dropdowns
+                      "~/Content/themes/arjuna/plugins/owl.carousel/owl-carousel/owl.theme.css",
+                      "~/Content/themes/arjuna/plugins/owl.carousel/owl-carousel/owl.transitions.css"
+                      ));            
+
+            // Arjuna scripts
+
+            bundles.Add(new ScriptBundle("~/bundles/arjuna").Include(
+                      "~/Content/themes/arjuna/js/arjuna.js" // arjuna
+                      ));
+                        
+            // RGraph component
+                                          
             bundles.Add(new ScriptBundle("~/bundles/rgraph").Include(
                     "~/Scripts/rgraph/libraries/RGraph.common.core.js",
                     "~/Scripts/rgraph/libraries/RGraph.common.dynamic.js",
@@ -50,6 +82,8 @@ namespace SurveyWeb
                     "~/Scripts/rgraph/libraries/RGraph.drawing.marker1.js",
                     "~/Scripts/rgraph/libraries/RGraph.bar.js",
                     "~/Scripts/rgraph/libraries/RGraph.gantt.js"));
+
+            // DropIt component for drop-down action menus
 
             bundles.Add(new StyleBundle("~/Content/dropdown").Include(
                       "~/Content/dropit.css"));
